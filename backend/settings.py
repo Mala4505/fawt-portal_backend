b7ea3ad8df07d10456bbee9e57cee6e4b15eca3c
Aliasger Mala <aliasgerzmala@gmail.com>
2025-11-07 01:22:47 +0300

    deploy

--- backend/settings.py
+++ backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-3jg61updz-j6nx2o)wr1)5#4ugeb+#70k=!yaflz+aisba*#o)
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "https://fawt-frontend.vercel.app/"
